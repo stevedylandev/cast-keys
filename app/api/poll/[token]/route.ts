cdcd7e512201bcca075bd127b3d4ccae31fb7548
Steve <stevedsimkins@gmail.com>
2024-10-03 01:33:14 -0400

    another

--- app/api/poll/[token]/route.ts
+++ app/api/poll/[token]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export const dynamic = "force";
+export const dynamic = "force-dynamic";
 
 export async function GET(
 	req: NextRequest,
