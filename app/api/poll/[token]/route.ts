93e95f8369e4d132afdcd8b3a9acf3de786f90ca
Steve <stevedsimkins@gmail.com>
2024-10-03 01:59:58 -0400

    trying revalidation

--- app/api/poll/[token]/route.ts
+++ app/api/poll/[token]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET(
 	req: NextRequest,
